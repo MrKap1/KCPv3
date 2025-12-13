2c31e52c460b1405092f16e4133f0a3823a8d903
Kap <karolgrz32@gmail.com>
2025-12-13 17:43:11 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,11 +11,11 @@ server_scripts {
 }
 
 files {
-  'mp_m_freemode_01_mp_m_kcpv3.meta',
-  'mp_f_freemode_01_mp_f_kcpv3.meta',
-  'stream/pedalternatevariations.meta'
+  'mp_m_freemode_01_mp_m_KCPv4_shop.meta',
+  'mp_f_freemode_01_mp_f_KCPv4_shop.meta',
+  'pedalternatevariations.meta'
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kcpv3.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kcpv3.meta'
-data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_KCPv4_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_KCPv4_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
